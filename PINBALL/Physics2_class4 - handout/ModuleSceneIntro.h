b9e6f14e32ccd9207d306f0f4a696527cc9e2ba3
Oriol de Dios <orioldedios@gmail.com>
2017-10-22 12:02:38 +0200

    Revert "Revert "lights sensors added""
    This reverts commit 2ba05a8948b93a4799074560e3298ee5b9731952.

--- PINBALL/Physics2_class4 - handout/ModuleSceneIntro.h
+++ PINBALL/Physics2_class4 - handout/ModuleSceneIntro.h
@@ -19,21 +19,38 @@ public:
 
 public:
 	p2List<PhysBody*> circles;
-	p2List<PhysBody*> boxes;
+	p2List<PhysBody*> balls;
 	p2List<PhysBody*> ricks;
 
-	PhysBody* sensor;
-	bool sensed;
+	PhysBody* sensor_star[3];
+	PhysBody* sensor_arrow_left[4];
+	PhysBody* sensor_arrow_right[4];
+	PhysBody* sensor_bonus_left;
+	PhysBody* sensor_bonus_right;
+	PhysBody* sensor_triangles[2];
+		
+	bool sensed_bonus_left = false;
+	bool sensed_bonus_right = false;
+	bool sensed_star[3] = { false,false,false };
+	bool sensed_arrow_left[4] = { false,false,false,false };
+	bool sensed_arrow_right[4] = { false,false,false,false };
 	bool colision_coin[3] = { false,false,false };
+	bool sensed_triangles[2] = { false,false };
 
 	SDL_Texture* circle;
+	SDL_Texture* ball;
 	SDL_Texture* box;
 	SDL_Texture* coin;
+	SDL_Texture* star;
 	SDL_Texture* back;
+	SDL_Texture* arrow_left;
+	SDL_Texture* arrow_right;
 	SDL_Texture* flipperL;
 	SDL_Texture* flipperR;
 
 	uint coin_fx;
-	p2Point<int> ray;
-	bool ray_on;
+	uint star_fx;
+	uint bonus_left_fx;
+	uint bonus_right_fx;
+	uint triangle_fx;
 };
